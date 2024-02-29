ca8faee9e492386a42103557801199d23a974b7f
Fabiola Veloz Pérez <velozfabiola04@outlook.com>
2024-02-29 10:14:38 -0600

    Version Inicial

---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById('startRecordingBtn').addEventListener('click', function() {
+    startRecording();
+});
+
+document.getElementById('stopRecordingBtn').addEventListener('click', function() {
+    stopRecording();
+});
+
+function startRecording() {
+    document.getElementById('startRecordingBtn').classList.add('d-none');
+    document.getElementById('stopRecordingBtn').classList.remove('d-none');
+
+    var recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+    recognition.lang = 'es-ES';
+    
+    recognition.onresult = function(event) {
+        var transcript = event.results[0][0].transcript;
+        document.getElementById('resultado').innerText = "Orden identificada: " + transcript;
+    };
+    
+    recognition.start();
+}
+
+function stopRecording() {
+    document.getElementById('startRecordingBtn').classList.remove('d-none');
+    document.getElementById('stopRecordingBtn').classList.add('d-none');
+}
