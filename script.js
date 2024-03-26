34299c07818aed7365dc37d8c4beb960717e2045
FabiolaVeloz <velozfabiola04@gmail.com>
2024-03-26 14:56:57 -0600

    Versión 1 Comandos Navegador

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-//Función para detectar la voz y escribir la información
-
-//Trae el id del texto a cambiar
-const controlTexto= document.getElementById('controlTexto');
+// Trae el id del texto a cambiar
+window.innerWidth = 800;
+window.innerHeight = 600;
+const controlTexto = document.getElementById('controlTexto');
 controlTexto.classList.add('fs-6');
 
-document.getElementById('startRecordingBtn').addEventListener('click', function() {
+document.getElementById('startRecordingBtn').addEventListener('click', function () {
     startRecording();
 });
 
-document.getElementById('stopRecordingBtn').addEventListener('click', function() {
+document.getElementById('stopRecordingBtn').addEventListener('click', function () {
     stopRecording();
 });
 
@@ -18,24 +18,58 @@ function startRecording() {
 
     const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
     recognition.lang = 'es-ES';
-    
-    recognition.onresult = function(event) {
+
+    recognition.onresult = function (event) {
         // Trae la información de todo lo que estuve hablando
         const transcript = event.results[0][0].transcript;
-        const keyword='tamaño 1';
         document.getElementById('resultado').innerText = "Orden identificada: " + transcript;
 
-        // Verificar si la palabra 'pollito' está en el resultado
-        if (transcript.toLowerCase().includes(keyword)) {
-            //Cambia el tamaño del texto
-            controlTexto.classList.add('fs-1');
-            console.log("Palabra encontrada");
+        // Verificar diferentes instrucciones reconocidas por voz usando switch
+        switch(true) {
+            case transcript.toLowerCase().includes('cambia a tamaño 1'):
+                // Cambia el tamaño del texto a 1
+                controlTexto.classList.add('fs-1');
+                break;
+            case transcript.toLowerCase().includes('abrir una ventana nueva'):
+                // Abre una nueva pestaña
+                window.open('about:blank', '_blank');
+                break;
+            case transcript.toLowerCase().includes('abre página del tec'):
+                // Abre la página del tec en una nueva ventana
+                window.open('https://itp.itpachuca.edu.mx/', '_blank');
+                break;
+            case transcript.toLowerCase().includes('cambia dimensiones'):
+                // Obtener la URL actual
+                const urlActual = window.location.href;
+                // Abrir una nueva ventana con la misma URL y dimensiones deseadas
+                const nuevaVentana = window.open(urlActual, '', 'width=800,height=600');
+                if (nuevaVentana) {
+                // Cerrar la ventana actual
+                window.close();
+                }
+                break;    
+            case transcript.toLowerCase().includes('cerrar una ventana'):
+                 window.open('', '_self', '');
+                 window.close();
+                 break
+            case transcript.toLowerCase().includes('cerrar el navegador'):
+                // Cierra el navegador
+                var ventanas = window.open('', '_self', '');
+                while (ventanas !== null) {
+                ventanas.close();
+                ventanas = window.open('', '_self', '');
+            }
+            break;
+            default:
+                // Instrucción no reconocida
+                console.log('Instrucción no reconocida');
         }
     };
 
-    recognition.onerror = function(event){
+    recognition.onerror = function (event) {
+        console.error('Error en el reconocimiento de voz: ', event.error);
     }
-    
+
     recognition.start();
 }
 
