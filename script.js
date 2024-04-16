b5381f4eac4eceec0807cfc73b94cfeddc7959eb
FabiolaVeloz <velozfabiola04@gmail.com>
2024-04-15 18:34:39 -0600

    Agregar Comando de reconocimiento 2

--- script.js
+++ script.js
@@ -11,71 +11,71 @@ function startRecording() {
         // Trae la información de todo lo que estuve hablando
         const transcript = event.results[0][0].transcript;
 
-        if (transcript.toLowerCase().includes('Fabiola')){
+        if (transcript.toLowerCase().includes('Fabiola')) {
 
-        
-        ordenIdentificadaDiv.innerText = "Orden Identificada: " + transcript;
+            ordenIdentificadaDiv.innerText = "Orden Identificada: " + transcript;
 
-        // Verificar diferentes instrucciones reconocidas por voz usando switch
-        switch(true) {
-            case transcript.toLowerCase().includes('abre una ventana nueva'):
-                // Abre una nueva pestaña
-                enviarDatosAMockAPI('Abre una ventana nueva');
-                window.open('about:blank', '_blank');
-                break;
-            case transcript.toLowerCase().includes('abre la página del tec'):
-                // Abre la página del tec en una nueva ventana
-                window.open('https://www.tecnm.mx/', '_blank');
-                enviarDatosAMockAPI('Abre la página del tec');
-                break;
-            case transcript.toLowerCase().includes('cambia dimensiones de la ventana'):
-            enviarDatosAMockAPI('Cambia dimensiones de la ventana').then(() => {
-            setTimeout(function() {
-            // Obtener la URL actual
-            const urlActual = window.location.href;
-            // Abrir una nueva ventana con la misma URL y dimensiones deseadas
-            const nuevaVentana = window.open(urlActual, '', 'width=800,height=600');
-            if (nuevaVentana) {
-                // Cerrar la ventana actual
-                window.close();
+            // Verificar diferentes instrucciones reconocidas por voz usando switch
+            switch (true) {
+                case transcript.toLowerCase().includes('abre una ventana nueva'):
+                    // Abre una nueva pestaña
+                    enviarDatosAMockAPI('Abre una ventana nueva');
+                    window.open('about:blank', '_blank');
+                    break;
+                case transcript.toLowerCase().includes('abre la página del tec'):
+                    // Abre la página del tec en una nueva ventana
+                    window.open('https://www.tecnm.mx/', '_blank');
+                    enviarDatosAMockAPI('Abre la página del tec');
+                    break;
+                case transcript.toLowerCase().includes('cambia dimensiones de la ventana'):
+                    enviarDatosAMockAPI('Cambia dimensiones de la ventana').then(() => {
+                        setTimeout(function () {
+                            // Obtener la URL actual
+                            const urlActual = window.location.href;
+                            // Abrir una nueva ventana con la misma URL y dimensiones deseadas
+                            const nuevaVentana = window.open(urlActual, '', 'width=800,height=600');
+                            if (nuevaVentana) {
+                                // Cerrar la ventana actual
+                                window.close();
+                            }
+                        }, 1000); // 1000 milisegundos = 1 segundo
+                    });
+                    break;
+                case transcript.toLowerCase().includes('cierra esta ventana'):
+                    enviarDatosAMockAPI('Cierra esta ventana').then(() => {
+                        setTimeout(function () {
+                            window.open('', '_self', '');
+                            window.close();
+                        }, 1000); // 1000 milisegundos = 1 segundo
+                    });
+                    break;
+                case transcript.toLowerCase().includes('dime la hora actual'):
+                    // Obtiene la hora actual
+                    enviarDatosAMockAPI('Hora actual');
+                    var fecha = new Date();
+                    var hora = fecha.getHours();
+                    var minutos = fecha.getMinutes();
+                    // Convierte la hora en formato legible
+                    var horaLegible = hora + ":" + (minutos < 10 ? '0' : '') + minutos;
+                    // Utiliza la API de Text-to-Speech para decir la hora
+                    var synth = window.speechSynthesis;
+                    var utterance = new SpeechSynthesisUtterance("La hora actual es " + horaLegible);
+                    synth.speak(utterance);
+                    break;
+                case transcript.toLowerCase().includes('consultar clima'):
+                    enviarDatosAMockAPI('Consultar clima');
+                    var ciudad = prompt("Por favor, ingresa la ciudad para buscar el clima en Google:");
+                    if (ciudad) {
+                        var urlGoogleClima = 'https://www.google.com/search?q=clima+' + ciudad;
+                        window.open(urlGoogleClima, '_blank');
+                    } else {
+                        alert("Debes ingresar una ciudad para buscar el clima.");
+                    }
+                    break;
+                default:
+                    // Instrucción no reconocida
+                    console.log('Instrucción no reconocida');
             }
-        }, 1000); // 1000 milisegundos = 1 segundo
-    });
-    break; 
-            case transcript.toLowerCase().includes('cierra esta ventana'):
-    enviarDatosAMockAPI('Cierra esta ventana').then(() => {
-        setTimeout(function() {
-            window.open('', '_self', '');
-            window.close();
-        }, 1000); // 1000 milisegundos = 1 segundo
-    });
-    break;
-            case transcript.toLowerCase().includes('dime la hora actual'):
-                // Obtiene la hora actual
-                enviarDatosAMockAPI('Hora actual');
-                var fecha = new Date();
-                var hora = fecha.getHours();
-                var minutos = fecha.getMinutes();
-                // Convierte la hora en formato legible
-                var horaLegible = hora + ":" + (minutos < 10 ? '0' : '') + minutos;
-                // Utiliza la API de Text-to-Speech para decir la hora
-                var synth = window.speechSynthesis;
-                var utterance = new SpeechSynthesisUtterance("La hora actual es " + horaLegible);
-                synth.speak(utterance);
-                break;
-            case transcript.toLowerCase().includes('consultar clima'):
-                enviarDatosAMockAPI('Consultar clima');
-                var ciudad = prompt("Por favor, ingresa la ciudad para buscar el clima en Google:");
-                if (ciudad) {
-                    var urlGoogleClima = 'https://www.google.com/search?q=clima+' + ciudad;
-                    window.open(urlGoogleClima, '_blank');
-                } else {
-                    alert("Debes ingresar una ciudad para buscar el clima.");
-                }
-                break;
-            default:
-                // Instrucción no reconocida
-                console.log('Instrucción no reconocida');
         }
     };
 
@@ -84,12 +84,11 @@ function startRecording() {
     }
 
     recognition.start();
-    
+
     // Reinicia la grabación cada 2 segundos
-    setInterval(function() {
+    setInterval(function () {
         recognition.start();
     }, 2000);
-}
 }
 
 function obtenerFechaHoraActual() {
@@ -119,7 +118,7 @@ function enviarDatosAMockAPI(instruccion) {
     const urlMockAPI = 'https://660c52433a0766e85dbdebe2.mockapi.io/comandos';
 
     // Enviar la solicitud POST
-    fetch(urlMockAPI, opciones)
+    return fetch(urlMockAPI, opciones)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Error en la solicitud POST a MockAPI');
