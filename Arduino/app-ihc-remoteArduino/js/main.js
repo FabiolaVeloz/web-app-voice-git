e2738902ab05f95834d4f0300023c6c29dbc1c61
FabiolaVeloz <velozfabiola04@gmail.com>
2024-04-20 15:49:12 -0600

    Arduino

---
+++ Arduino/app-ihc-remoteArduino/js/main.js
@@ -0,0 +1,69 @@
+let ultimoDatoAnterior = null; // Variable para almacenar el último dato anterior
+setInterval(recibirDatos, 2000);
+
+async function recibirDatos() {
+    try {
+        // Opciones de la solicitud
+        const opciones = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+
+        const url = 'https://6624267404457d4aaf9bbc2d.mockapi.io/comandos';
+        // Recibir los datos de la URL proporcionada
+        const response = await fetch(url, opciones);
+
+        if (!response.ok) {
+            throw new Error('Error en la solicitud GET a la URL');
+        }
+
+        const data = await response.json();
+        console.log('Datos recibidos:', data);
+        // Arreglo
+        datosRecibidos = data;
+        // Último dígito del arreglo
+        const ultimoDato = datosRecibidos.pop();
+        console.log('Último dato:', ultimoDato);
+
+        // Verifica si el último dato ha cambiado
+        if (!esIgual(ultimoDato, ultimoDatoAnterior)) {
+            // Sustitución en los párrafos
+            const parrafoOrdenRecibida = document.getElementById('OrdenRecibida');
+            parrafoOrdenRecibida.textContent = ultimoDato.instruccion;
+            const parrafoHora = document.getElementById('Hora');
+            parrafoHora.textContent = ultimoDato.fechaHora;
+            // Llama a la función para manejar las acciones basadas en el último dato
+            manejarAccion(ultimoDato);
+            // Actualiza el valor del último dato anterior
+            ultimoDatoAnterior = ultimoDato;
+        }
+
+        
+        
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+function esIgual(objA, objB) {
+    return JSON.stringify(objA) === JSON.stringify(objB);
+}
+
+function manejarAccion(ultimoDato) {
+    switch (ultimoDato.instruccion.toLowerCase()) {
+        case 'muestra imagen feliz':
+            document.getElementById('microfono-image').src = 'feliz.jpg';
+            break;
+        case 'muestra imagen triste':
+            document.getElementById('microfono-image').src = 'triste.jpg';
+            break;
+        
+        default:
+            // Instrucción no reconocida
+            console.log('Instrucción no reconocida');
+    }
+}
+
---
+++ Arduino/script.js
@@ -0,0 +1,102 @@
+window.innerWidth = 800;
+window.innerHeight = 600;
+
+let recognition; // Variable para almacenar el objeto de reconocimiento de voz
+let restartInterval; 
+
+function startRecording() {
+    document.getElementById('microfono-image').src = 'microfono-encendido.png';
+    document.getElementById('microfono-image').style.animation = 'encender 1.3s ease-in-out infinite alternate';
+
+    recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition || window.mozSpeechRecognition || window.msSpeechRecognition)();
+    recognition.lang = 'es-ES';
+    const ordenIdentificada = document.getElementById('ordenIdentificada');
+    recognition.onresult = function (event) {
+        // Trae la información de todo lo que estuve hablando
+        const transcript = event.results[0][0].transcript;
+        if (transcript.toLowerCase().includes('comandos')) {
+
+            ordenIdentificada.textContent = "Orden Identificada: " + transcript;
+            document.querySelectorAll('.imagen-oculta').forEach(function(img) {
+                img.classList.remove('imagen-oculta');
+            });
+            // Verificar diferentes instrucciones reconocidas por voz usando switch
+            switch (true) {
+                case transcript.toLowerCase().includes('muestra imagen feliz'):
+                    enviarDatosAMockAPI('Muestra imagen feliz');
+                    document.getElementById('imagen').src = 'feliz.jpg';
+                    break;
+                    case transcript.toLowerCase().includes('muestra imagen triste'):
+                    document.getElementById('imagen').src = 'triste.jpg';
+                    enviarDatosAMockAPI('Muestra imagen triste');
+                    break;
+                default:
+                    // Instrucción no reconocida
+                    console.log('Instrucción no reconocida');
+            }
+        }
+    };
+
+    recognition.onerror = function (event) {
+        console.error('Error en el reconocimiento de voz: ', event.error);
+    }
+
+    recognition.start();
+
+    // Reinicia la grabación cada 5 segundos
+    restartInterval = setInterval(function () {
+        recognition.start();
+    }, 2000);
+}
+
+function stopRecording() {
+    if (recognition) {
+        document.getElementById('microfono-image').src = 'microfono-apagado.png';
+        document.getElementById('microfono-image').style.animation = 'none';
+        ordenIdentificada.textContent = "Orden Identificada: ";
+        recognition.stop();
+        clearInterval(restartInterval); // Detiene el intervalo de reinicio
+    }
+}
+
+function obtenerFechaHoraActual() {
+    return new Date().toLocaleString();
+}
+
+// Función para enviar datos a MockAPI
+function enviarDatosAMockAPI(instruccion) {
+    const fechaHoraActual = obtenerFechaHoraActual();
+
+    // Datos a enviar en la solicitud POST
+    const datos = {
+        instruccion: instruccion,
+        fechaHora: fechaHoraActual
+    };
+
+    // Opciones de la solicitud
+    const opciones = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(datos)
+    };
+
+    // URL de MockAPI
+    const urlMockAPI = 'https://6624267404457d4aaf9bbc2d.mockapi.io/comandos';
+
+    // Enviar la solicitud POST
+    return fetch(urlMockAPI, opciones)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error en la solicitud POST a MockAPI');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Registro exitoso en MockAPI:', data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+}
